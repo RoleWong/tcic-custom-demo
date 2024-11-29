6762d1c0e9a03469e92fb3d2b7132ed6436e83f1
shuaisguo <shuaisguo@tencent.com>
2024-11-29 17:22:48 +0800

    feat: 梳理项目结构

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### 低代码课堂自定义 DEMO
 
 
-- [学生白板权限控制](./whiteBoardAuth/index.js)
+- [学生白板权限控制](./demos/whiteBoardAuth)
---
+++ demos/whiteBoardAuth/src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+document.querySelector("#root").innerHTML = `
+<div class="content">
+  <h1>Vanilla Rspack</h1>
+  <p>Start building amazing things with Rspack.</p>
+</div>
+`;
