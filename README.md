30f296ccedd328d990fa5a3fd89957788b4c57df
shuaisguo <shuaisguo@tencent.com>
2024-12-13 11:34:40 +0800

    feat: 添加 demo

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 - [学生白板权限控制](./demos/whiteBoardAuth)
+- [自定义定时器铃声](./demos/whiteBoardAuth)
+- [下课前 5 分钟弹窗提醒](./demos/endTimeModal)
--- demos/whiteBoardAuth/src/index.js
+++ demos/whiteBoardAuth/src/index.js
@@ -3,7 +3,7 @@ TCIC.SDK.instance
   .then(() => {
     // 如果是学生，隐藏他的添加白板，选择白板权限
     if (TCIC.SDK.instance.isStudent()) {
-      TCIC.SDK.instance.setFeatureAvailable("WhiteBoardList", false);
+      TCIC.SDK.instance.setFeatureAvailable("WhiteBoardList", true);
       TCIC.SDK.instance.setFeatureAvailable("WhiteBoardPPT", false);
     }
   });
