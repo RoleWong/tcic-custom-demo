d515cd25d54f61b13da76a0e0792bdc32e526714
shuaisguo <shuaisguo@tencent.com>
2025-03-03 18:51:47 +0800

    chore: update demo

--- demos/mp-navback/src/index.js
+++ demos/mp-navback/src/index.js
@@ -2,8 +2,7 @@ TCIC.SDK.instance
   .promiseState(TCIC.TMainState.Class_Status, TCIC.TClassStatus.Already_Start)
   .then(() => {
     TCIC.SDK.instance.showMessageBox('温馨提示', '点击返回小程序上一页', ['确定','取消'] , () => {
-      // 注意不要用 wx.miniProgram.navigateBack
-      jWeixin.miniProgram.navigateBack({
+      window.wx.miniProgram.navigateBack({
         delta: 1
       });
     })
--- demos/whiteBoardExport/src/index.js
+++ demos/whiteBoardExport/src/index.js
@@ -1,18 +1,17 @@
+
 TCIC.SDK.instance
-  .promiseState(TCIC.TMainState.Class_Status, TCIC.TClassStatus.Already_Start)
-  .then(() => {
-    /* 生成板书图片
-    * https://doc.qcloudtiw.com/web/official/TEduBoard.html#addSnapshotMark
-    */
-    const teduBoard = TCIC.SDK.instance.getBoard();
-    const TEduBoard = window.TEduBoard;
-    teduBoard.on(TEduBoard.EVENT.TEB_ADDBOARD, () => {
-      teduBoard.addSnapshotMark();
-    });
-    teduBoard.on(TEduBoard.EVENT.TEB_DELETEBOARD, () => {
-      teduBoard.addSnapshotMark();
-    });
-    teduBoard.on(TEduBoard.EVENT.TEB_GOTOBOARD, () => {
-      teduBoard.addSnapshotMark();
-    });
+.promiseState(TCIC.TMainState.Class_Status, TCIC.TClassStatus.Already_Start)
+.then(() => {
+  const teduBoard = TCIC.SDK.instance.getBoard();
+  /* 生成板书图片
+  * https://doc.qcloudtiw.com/web/official/TEduBoard.html#addSnapshotMark
+  */
+  TCIC.SDK.instance.setFeatureAvailable('WhiteBoardSnapShot', true);
+  TCIC.SDK.instance.on(TCIC.TMainEvent.Before_Leave, () => {
+    teduBoard.addSnapshotMark();
   });
+  window.onbeforeunload = () => {
+    teduBoard.addSnapshotMark();
+  };
+});
+  
